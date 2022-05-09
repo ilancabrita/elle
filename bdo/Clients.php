540e7ab8befd202421fcc5244214db81b935938b
ilancabrita <82396649+ilancabrita@users.noreply.github.com>
2022-05-09 12:22:40 -0300

    maj 1 connect BDO 22.05.09

---
+++ bdo/Clients.php
@@ -0,0 +1,90 @@
+<?php
+// use Clients;
+
+class Clients extends ConnectionBDO
+{
+    private function __construct() {
+        
+    }
+
+    public function insert($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("INSERT INTO clients (adresse, codePostale, ville, pays, telephone) VALUES (?,?,?,?,?)");
+            $res->bindParam(1, $data['adresse'], PDO::PARAM_STR);
+            $res->bindParam(2, $data['codePostale'], PDO::PARAM_STR);
+            $res->bindParam(3, $data['ville'], PDO::PARAM_STR);
+            $res->bindParam(4, $data['pays'], PDO::PARAM_STR);
+            $res->bindParam(5, $data['telephone'], PDO::PARAM_STR);
+            return $res->execute();
+        }catch (Exception $e){
+           die("Error User->clients() " . $e->getMessage());
+        }
+    }
+
+    public function update($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("UPDATE adresse = ?, codePostale = ?, ville = ?, pays = ?, telephone = ? SET  WHERE idClientsClients = ?");
+            $res->bindParam(1, $data['adresse'], PDO::PARAM_STR);
+            $res->bindParam(2, $data['codePostale'], PDO::PARAM_STR);
+            $res->bindParam(3, $data['ville'], PDO::PARAM_STR);
+            $res->bindParam(4, $data['pays'], PDO::PARAM_STR);
+            $res->bindParam(6, $data['telephone'], PDO::PARAM_STR);
+            $res->bindParam(6, $data['idClients'], PDO::PARAM_INT);
+            return $res->execute();
+        }catch (Exception $e){
+            die("Error User->clients() " . $e->getMessage());
+        }
+    }
+
+    public function delete($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("DELETE clients WHERE idClients = ?");
+            $res->bindParam(1, $data['idClients'], PDO::PARAM_INT);
+            return $res->execute();
+        }catch (Exception $e){
+            die("Error User->clients() " . $e->getMessage());
+        }
+    }
+
+    public function selectAll($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("SELECT * FROM clients");
+            $res->execute();
+            return $res->fetchAll();
+        }catch (Exception $e){
+            die("Error User->clients() " . $e->getMessage());
+        }
+    }
+
+    public function selectByidClients($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("SELECT * FROM clients WHERE idClients = ? ");
+            $res->bindParam(1, $data['idClients'], PDO::PARAM_INT);
+            $res->execute();
+            return $res->fetch();
+        }catch (Exception $e){
+            die("Error User->clients() " . $e->getMessage());
+        }
+    }
+
+    public function test()
+    {
+        echo "fonctionne";
+    }
+
+    /**
+     * TODO:
+     * 1 criar un metodo inserir
+     * 2 criar un metodo update
+     * 3 criar un metodo delete
+     * 4 criar un metodo select all
+     * 5 criar un metodo select by idClients
+     */
+}
+
+?>
---
+++ bdo/Prestataire.php
@@ -0,0 +1,84 @@
+<?php
+// use Pretataire;
+
+class Prastataire extends ConnectionBDO
+{
+    private function __construct() {
+        
+    }
+
+    public function insert($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("INSERT INTO prestataire (jourDeTravail, horaireDeTravail) VALUES (?,?)");
+            $res->bindParam(1, $data['jourDeTravail'], PDO::PARAM_STR);
+            $res->bindParam(2, $data['horaireDeTravail'], PDO::PARAM_STR);
+            return $res->execute();
+        }catch (Exception $e){
+           die("Error User->prestataire() " . $e->getMessage());
+        }
+    }
+
+    public function update($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("UPDATE jourDeTravail = ?, horaireDeTravail = ? SET  WHERE idPrestataire = ?");
+            $res->bindParam(1, $data['jourDeTravail'], PDO::PARAM_STR);
+            $res->bindParam(2, $data['horaireDeTravail'], PDO::PARAM_STR);
+            $res->bindParam(3, $data['idPrestataire'], PDO::PARAM_INT);
+            return $res->execute();
+        }catch (Exception $e){
+            die("Error User->prestataire() " . $e->getMessage());
+        }
+    }
+
+    public function delete($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("DELETE prestataire WHERE idPrestataire = ?");
+            $res->bindParam(1, $data['idPrestataire'], PDO::PARAM_INT);
+            return $res->execute();
+        }catch (Exception $e){
+            die("Error User->prestataire() " . $e->getMessage());
+        }
+    }
+
+    public function selectAll($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("SELECT * FROM prestataire");
+            $res->execute();
+            return $res->fetchAll();
+        }catch (Exception $e){
+            die("Error User->prestataire() " . $e->getMessage());
+        }
+    }
+
+    public function selectByidPrestataire($data)
+    {
+        try{
+            $res = parent::getInstance()->prepare("SELECT * FROM prestataire WHERE idPrestataire = ? ");
+            $res->bindParam(1, $data['idPrestataire'], PDO::PARAM_INT);
+            $res->execute();
+            return $res->fetch();
+        }catch (Exception $e){
+            die("Error User->prestataire() " . $e->getMessage());
+        }
+    }
+
+    public function test()
+    {
+        echo "fonctionne";
+    }
+
+    /**
+     * TODO:
+     * 1 criar un metodo inserir
+     * 2 criar un metodo update
+     * 3 criar un metodo delete
+     * 4 criar un metodo select all
+     * 5 criar un metodo select by idPrestataire
+     */
+}
+
+?>
