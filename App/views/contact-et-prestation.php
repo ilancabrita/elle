1716531d3518c22f2a267ba55f6c4ee20a9dfc1f
ilancabrita <82396649+ilancabrita@users.noreply.github.com>
2022-07-13 00:17:49 +0200

    maj 22.07.12

--- App/views/contact-et-prestation.php
+++ App/views/contact-et-prestation.php
@@ -17,40 +17,7 @@
     <div>TOUT NOS COMPETENCES DE BEAUTE SONT SUR ELLE EMBELLIT 95</div>
     <div>Soins du visage 100% naturels, peaux atones, peaux matures, peaux jeunes, asphyxiées, sensibles et déshydratées. Soins du corps 100% naturels : anti-stress, relaxantant, amincissant et tonifiant. Maquillage de mariée ou de soirée. Teinture de cils et de sourcils. Manicure et beauté des pieds. Des forfaits de soins et d'abonement vous permettent de bénéficier de terifs avantageux pour moduler vote bien-être en toute détente dés aujourd'hui !</div>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Service</th>
-                <th>Prix</th>
-            </tr>
-        </thead>
-
-        <tbody>
-
-            <?php 
-            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
-            ?>
-
-            <tr>
-                <td>
-                    <?php echo 
-                    htmlspecialchars($row['service']);
-                    ?>
-                </td>
-
-                <td>
-                    <?php echo 
-                    htmlspecialchars($row['prix']);
-                    ?>
-                </td>
-            </tr>
-            
-            <?php 
-            endwhile;
-            ?>
-
-        </tbody>
-    </table>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2616.666570255955!2d2.246095815915877!3d49.016928097484346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e65d8a1935259d%3A0x3fc96b44701bd036!2sElle%20embellit!5e0!3m2!1sfr!2sfr!4v1657618091771!5m2!1sfr!2sfr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
     <footer>
 
